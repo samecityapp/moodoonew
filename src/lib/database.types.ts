eac73f6741b1e9c84438460ff1b0201ac876e07f
samecityapp <erdempakyollu@gmail.com>
2025-11-28 22:10:55 +0300

    Updated database.types.ts

--- src/lib/database.types.ts
+++ src/lib/database.types.ts
@@ -19,6 +19,7 @@ export interface Database {
           images: string[]
           shopier_url: string
           dimensions: string | null
+          materials: string | null
           is_featured: boolean
           stock: number
         }
@@ -31,6 +32,7 @@ export interface Database {
           images: string[]
           shopier_url: string
           dimensions?: string | null
+          materials?: string | null
           is_featured?: boolean
           stock?: number
         }
@@ -43,6 +45,7 @@ export interface Database {
           images?: string[]
           shopier_url?: string
           dimensions?: string | null
+          materials?: string | null
           is_featured?: boolean
           stock?: number
         }
